f64b5a115c725fae7dc43b642de97d7d542ba19b
Vishal Kumar <148609343+Kumar-VishalJai-swal@users.noreply.github.com>
2024-03-20 13:15:59 +0530

    members.js

---
+++ members.js
@@ -0,0 +1,375 @@
+var arr = [
+
+    // _________________________ chetan _________________________________________________
+    {dp:"./image/chetan.jpg", 
+    name:"Chetan Kshirsagor"
+
+    ,linkedin: "https://www.linkedin.com/in/chetan-kshirsagar87?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    
+    leetcode: "",
+    leet: "",
+    leetcodeimg: "",
+
+    insta: "",
+     instaicon:``,
+     instagram: "",
+
+     hackerrank: "",
+     hackerrankicon: ``,
+     hacker:"",
+
+     twitter: "https://x.com/chetan_k87?t=6ZZphpZ50vRFWui4VL1ELw&s=08",
+     twittericon: `<i class="fa-brands fa-x-twitter"></i>`,
+     twitt: "Twiiter",
+
+     github: "https://github.com/CH3T4N87/CH3T4N87",
+    email: "vkjaiswal1419@gmail.com",
+    skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+    aboutme: `Hi ,
+    I am a passionate learner. Currently I am learning MERN Stack and DSA in Java.`},
+
+
+
+    // __________________________ dakshata ______________________________________________
+
+    {dp:"https://cdn4.sharechat.com/compressed_gm_40_img_88920_2ad8c31a_1694783086423_sc.jpg?tenant=sc&referrer=tag-service&f=423_sc.jpg", 
+    name:"Dakshata Ramteke"
+
+    ,linkedin: "https://www.linkedin.com/in/dakshata-ramteke-20689126a/",
+    
+    leetcode: "",
+     leet: "",
+     leetcodeimg: "",
+
+     twitter: "",
+     twittericon: ``,
+     twitt: "",
+
+     hackerrank: "",
+     hackerrankicon: ``,
+     hacker:"",
+
+     insta: "",
+     instaicon:``,
+     instagram: "",
+
+     github: "https://github.com/dakshataramteke",
+     email: "coolrey1512@gmail.com",
+     skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+    aboutme: ""},
+
+
+
+    // ___________________________ renu bakshi ________________________________________
+
+    {dp:"https://cdn4.sharechat.com/img_183306_1b9043f0_1672231180921_sc.jpg?tenant=sc&referrer=tag-service&f=921_sc.jpg", 
+    name:"Renu Bakshi",
+    
+    linkedin: "https://www.linkedin.com/in/renu-bakshi/",
+
+    leetcode: "",
+    leet: "",
+    leetcodeimg: "",
+
+    insta: "",
+     instaicon:``,
+     instagram: "",
+
+     twitter: "",
+     twittericon: ``,
+     twitt: "",
+
+     hackerrank: "",
+     hackerrankicon: ``,
+     hacker:"",
+
+     
+     discordlink:"https://discord.com/channels/@me/1219158457744232459",
+   
+
+     github: "https://github.com/Renubakshi",
+
+     email: "vkjaiswal1419@gmail.com",
+
+     skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+    aboutme: `ex-NIC intern   Full Stack Developer (soon to be MERN)  Web Developer || Solved 100+ problems. <br>
+
+      Hello, my name is Renu Bakshi. I am  a dedicated Full Stack Developer expertise in a variety of technologies like  HTML, CSS, JavaScript, Bootstrap, Node.js, Express.js, and MongoDB. 
+      With a strong foundation in front-end development, I excel in crafting pixel-perfect user interfaces that prioritize usability and accessibility. My expertise extends to the back end, where I leverage Node.js and Express.js to build efficient and scalable server-side applications. Proficient in database management with MongoDB, I thrive on solving technical challenges and delivering high-quality solutions.
+      Let's collaborate and turn your ideas into reality. Get in touch, and let's discuss how I can contribute to the success of your projects.`},
+
+
+
+
+    // ______________________________ loriex __________________________________
+
+    {dp:"https://pics.craiyon.com/2023-06-06/19d2a7c08cd6439193d53b28cfd721a9.webp", 
+    name:"Loriex"
+
+    ,linkedin: "https://www.linkedin.com/in/jaiswal-vishal2024/",
+
+     leetcode: "",
+     leet: "",
+     leetcodeimg: "",
+
+     hackerrank: "",
+     hackerrankicon: ``,
+     hacker:"",
+
+     insta: "",
+     instaicon:``,
+     instagram: "",
+
+     github: "https://github.com/iamloriex",
+     email: "vkjaiswal1419@gmail.com",
+     skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+
+     twitter: "",
+     twittericon: ``,
+     twitt: "",
+
+    aboutme: ""},
+
+
+
+    // ___________________________ jay govind ______________________________
+
+    {dp:"./image/member-jay_govind.webp", 
+    name:"Jay Govind"
+
+    ,linkedin: "https://www.linkedin.com/in/govind-jay/",
+
+    leetcode: "https://leetcode.com/Jay-Govind/",
+    leetcodeimg: "./image/leetcode.png",
+    leet: "leetcode",
+
+    hackerrank: "https://www.hackerrank.com/profile/Jay_Govind",
+     hackerrankicon: `<i class="fa-brands fa-hackerrank"></i>`,
+     hacker:"HackerRank",
+
+     email: "",
+
+     twitter: "https://twitter.com/_JayGovind",
+     twittericon: `<i class="fa-brands fa-x-twitter"></i>`,
+     twitt: "Twitter",
+
+     insta: "https://www.instagram.com/devwithgovind/",
+     instaicon:`<i class="fa-brands fa-instagram"></i>`,
+     instagram: "Instagram",
+
+     github: "https://github.com/Jay-Govind",
+     skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+    aboutme: `Hello! I'm Jay Govind 😀, <br>
+
+    🔰 Experienced as a Learning Facilitator at Bihar Skill Development Mission, where I helped 1K+ students to understand their common objectives and plan to achieve them. <br>
+    
+    🔰 Currently mastering frameworks like Bootstrap, Tailwind CSS, and the MERN Stack (MongoDB, Express.js, React, Node.js).I am actively honing my skills to become proficient in crafting seamless and engaging web experiences. <br>
+    
+    🔰 Skillfull in HTML5, CSS3, JavaScript, C, C++(Basic) and had done several projects. <br>
+    
+    🔰 Aspiring for a challenging career in Full Stack Development as to use my learned skills and experience for best results.
+    
+    Happy Coding 😊`},
+
+
+    // ___________________________ vishal jaiswal __________________________
+
+    {dp:"./image/member-vishal.jpeg", 
+    name:"Vishal Jaiswal"
+    ,linkedin: "https://www.linkedin.com/in/jaiswal-vishal2024/",
+
+     leetcode: "https://leetcode.com/Vishal_Kumar1419/",
+     leetcodeimg: "./image/leetcode.png",
+     leet: "leetcode",
+
+     github: "https://github.com/vishaljaiswal2343",
+
+     email: "vkjaiswal1419@gmail.com",
+
+     twitter: "https://twitter.com/vishaljais1419",
+     twittericon: `<i class="fa-brands fa-x-twitter"></i>`,
+     twitt: "Twitter",
+
+     insta: "https://www.instagram.com/vishaljaiswal2343/",
+     instaicon:`<i class="fa-brands fa-instagram"></i>`,
+     instagram: "Instagram",
+
+     hackerrank: "https://www.hackerrank.com/profile/vishalkumarjais2",
+     hackerrankicon: `<i class="fa-brands fa-hackerrank"></i>`,
+     hacker:"HackerRank",
+
+     skills: ["Node.js","React.js","Express.js","MongoDB","JavaScript","C++","Java","TypeScript","HTML","CSS","MySQL","REST API","Tailwind CSS","Bootstrap","Git","GitHub"],
+    aboutme: `Experienced Web Interaction Developer with a keen eye for
+    quality, enhanced UI & UX, quick learning abilities focused 
+    on problem solving and critical thinking. Would love engaging
+     in challenging projects that provide opportunity to expand 
+     knowledge and explore new technologies. Expertise in JavaScript 
+     technologies across Nodejs, Angular 12, ReactJs. Experienced in 
+     building complete optimized web application using data structures 
+     and algorithms.`},
+
+   
+]
+
+
+var clutter = ""
+arr.forEach(function(elem,idx){
+    clutter += `<div id="member-card-${idx}" class="member-card">
+    <div class="member-image">
+    <img id="${idx}" src="${elem.dp}" alt="">
+</div>
+<h2>${elem.name}</h2>
+<p>Full Stack Developer</p>
+<div class="social-links">
+    <div class="links">
+        <a href="${elem.linkedin}"><i id="linkedin" class="fa-brands fa-linkedin"></i></a>
+        
+    </div>
+    <div class="links">
+        <a href="${elem.discordlink}"><i class="fa-brands fa-discord"></i></a>
+        
+    </div>
+    <div class="links">
+        <a href="${elem.github}"><i class="fa-brands fa-github"></i></a>
+        
+    </div>
+    
+
+</div>
+
+<button id="view_more-${idx}"  class="view-more" >View More</button>
+    </div>`
+})
+document.querySelector("#members").innerHTML = clutter
+
+
+
+arr.forEach(function(elem,idx){
+    document.querySelector(`#view_more-${idx}`).addEventListener("click", function(dats){
+        document.querySelector("#main_container").style.display = "flex";
+        document.querySelector("#members").style.display = "none";
+        document.querySelector("#back_arrow").style.display = "inline-block";
+        document.querySelector("#main_container").innerHTML = `<div id="profile-info" class="profile-info">
+        <div class="profile-img">
+            <img src="${elem.dp}" alt="">
+        </div>
+        <p class="profile-name">${elem.name}</p>
+        <p class="flull-stack">Full-Stack-Devwloper</p>
+
+        <div class="line"></div>
+
+        <div class="other-info">
+            <div class="inbox">
+                <i class="fa-solid fa-handshake"></i>
+                <a href="#">Collaborate</a>
+            </div>
+            <div class="sent">
+                <i class="fa-solid fa-paper-plane"></i>
+                <a href="#">Sent</a>
+            </div>
+            <div class="links">
+                <i class="fa-solid fa-envelope"></i>
+                <a href="${elem.email}">Email</a>
+            </div>
+            
+            <div class="line"></div>
+
+            <div class="sent">
+                <i class="fa-regular fa-share-from-square"></i>
+                <a href="#">Share profile</a>
+            </div>
+            <div class="sent">
+            <i class="fa-solid fa-diagram-project"></i>
+                <a href="#">All Projects</a>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="resume">
+                <i class="fa-solid fa-cloud-arrow-down"></i>
+                <a href="#">Resume</a>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div id="about-skill" class="about-skill">
+        <div class="about-me">
+            <h2 class="about-heading">About me</h2>
+            <p>${elem.aboutme}</p>
+        </div>
+        <div class="line"></div>
+        <p class="account">Links</p>
+
+            <div class="social-midea-links">
+                <div class="left-links">
+                    <div class="links">
+                        <i class="fa-brands fa-linkedin"></i>
+                        <a href="${elem.linkedin}">LinkedIn</a>
+                    </div>
+                    
+                    <div class="links">
+                        <img src="${elem.leetcodeimg}" width="20px" alt="">
+                        <a href="${elem.leetcode}">${elem.leet}</a>
+                    </div>
+                    <div class="links">
+                        ${elem.twittericon}
+                        <a href="${elem.twitter}">${elem.twitt}</a>
+                    </div>
+                </div>
+                <div class="right-links">
+                <div class="links">
+                        <i class="fa-brands fa-github"></i>
+                        <a href="${elem.github}">GitHub</a>
+                    </div>
+                    
+    
+                    <div class="links">
+                        ${elem.instaicon}
+                        <a href="${elem.insta}">${elem.instagram}</a>
+                    </div>
+    
+                    <div class="links">
+                        ${elem.hackerrankicon}
+                        <a href="${elem.hackerrank}">${elem.hacker}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <p class="account">skills</p>
+            <div class="skills">
+                <span class="skill-name">${elem.skills[0]}</span>
+                <span class="skill-name">${elem.skills[1]}</span>
+                <span class="skill-name">${elem.skills[2]}</span>
+                <span class="skill-name">${elem.skills[3]}</span>
+                <span class="skill-name">${elem.skills[4]}</span>
+                <span class="skill-name">${elem.skills[5]}</span>
+                <span class="skill-name">${elem.skills[6]}</span>
+                <span class="skill-name">${elem.skills[7]}</span>
+                <span class="skill-name">${elem.skills[8]}</span>
+                <span class="skill-name">${elem.skills[9]}</span>
+                <span class="skill-name">${elem.skills[10]}</span>
+                <span class="skill-name">${elem.skills[11]}</span>
+                <span class="skill-name">${elem.skills[12]}</span>
+                <span class="skill-name">${elem.skills[13]}</span>
+                <span class="skill-name">${elem.skills[14]}</span>
+                <span class="skill-name">${elem.skills[15]}</span>
+               
+
+                
+                
+            </div>
+    </div>`
+
+    });
+});
+
+document.querySelector("#back_arrow").addEventListener("click", function(dats){
+    document.querySelector("#main_container").style.display = "none";
+    document.querySelector("#members").style.display = "flex";
+    document.querySelector("#profile-info").style.display = "none";
+    document.querySelector("#about-skill").style.display = "none";
+    document.querySelector("#back_arrow").style.display = "none";
+
+});
